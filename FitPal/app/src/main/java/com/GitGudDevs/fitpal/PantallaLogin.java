074e8867bc28ad329e8da3ad4c1494bcb4ae1010
pablo-savp <santanderlavarez.pablo@gmail.com>
2022-03-09 01:10:29 -0500

    PantallaLogin arreglada

--- FitPal/app/src/main/java/com/GitGudDevs/fitpal/PantallaLogin.java
+++ FitPal/app/src/main/java/com/GitGudDevs/fitpal/PantallaLogin.java
@@ -9,14 +9,15 @@ import android.widget.Button;
 
 public class PantallaLogin extends AppCompatActivity {
     Button login;
-
+    Button registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_login);
+        setContentView(R.layout.activity_pantalla_login1);
         login=findViewById(R.id.ingreso);
+        registro=findViewById(R.id.registro);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(PantallaLogin.this, PantallaRegistro1.class);
