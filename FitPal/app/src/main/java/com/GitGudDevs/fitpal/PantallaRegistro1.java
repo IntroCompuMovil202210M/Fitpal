b9c94879e679f5f18dfcb15e3b7db114d059d974
pablo-savp <santanderlavarez.pablo@gmail.com>
2022-03-08 23:49:16 -0500

    Intents de Login a registro1 y viceveresa

--- FitPal/app/src/main/java/com/GitGudDevs/fitpal/PantallaRegistro1.java
+++ FitPal/app/src/main/java/com/GitGudDevs/fitpal/PantallaRegistro1.java
@@ -2,13 +2,30 @@ package com.GitGudDevs.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PantallaRegistro1 extends AppCompatActivity {
+    Button siguiente;
+    Button anterior;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro1);
+        siguiente=findViewById(R.id.siguiente1);
+        anterior=findViewById(R.id.anterior1);
+
+        anterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(PantallaRegistro1.this, PantallaLogin.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
