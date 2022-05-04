0cfac3db14ce0c888ccb1f2d69782e807666c042
richfon <58142219+richfon@users.noreply.github.com>
2022-05-04 05:27:18 -0500

    parte perfil, aun no conectado

--- FitPal/app/src/main/java/com/gitgud/fitpal/MiPerfil.java
+++ FitPal/app/src/main/java/com/gitgud/fitpal/MiPerfil.java
@@ -2,13 +2,32 @@ package com.gitgud.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MiPerfil extends AppCompatActivity {
-
+    ImageView imagen;
+    ImageButton img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_mi_perfil);
+        img=findViewById(R.id.confi);
+        imagen.setImageResource(R.drawable.viga);
+        img.setOnClickListener(openActivity);
     }
+    private View.OnClickListener openActivity = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MiPerfil.this, ModificarPerfil.class);
+            startActivity(intent);
+        }
+    };
+
+
 }
