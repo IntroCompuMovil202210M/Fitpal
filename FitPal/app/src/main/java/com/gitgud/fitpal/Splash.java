72173eb185c9c78d129440b8ec69d7841dac3743
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2022-05-04 03:12:05 -0500

    prueba

--- FitPal/app/src/main/java/com/gitgud/fitpal/Splash.java
+++ FitPal/app/src/main/java/com/gitgud/fitpal/Splash.java
@@ -10,13 +10,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import com.gitgud.fitpal.entidades.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.ArrayList;
 
 public class Splash extends AppCompatActivity {
 
@@ -66,6 +72,49 @@ public class Splash extends AppCompatActivity {
                                     }
                                 }
                             });
+
+                    //borrar esto despues
+
+                    String correo = user.getEmail();
+                    ArrayList<String> eventos = new ArrayList<>();
+                    ArrayList<Usuario> usuarios = null;
+                    db.collection("usuarios")
+                            .whereEqualTo("correo", correo)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            usuarios.add(document.toObject(Usuario.class));
+                                        }
+                                    } else {
+                                        Log.d(TAG, "Error getting documents: ", task.getException());
+                                    }
+                                }
+                            });
+                    eventos = usuarios.get(0).getEventos();
+
+                    for(String evento: eventos){
+                        DocumentReference docRef = db.collection("eventos").document(evento);
+
+                        Source source = Source.CACHE;
+
+                    // Get the document, forcing the SDK to use the offline cache
+                        docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    // Document found in the offline cache
+                                    DocumentSnapshot document = task.getResult();
+                                    Log.d(TAG, "Cached document data: " + document.getData());
+                                } else {
+                                    Log.d(TAG, "Cached get failed: ", task.getException());
+                                }
+                            }
+                        });
+                    }
+                    //borrar esto despues
                 }
 
             }
