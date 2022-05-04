d4fb50174d709a7855f3b716ff259496c88cb8d8
StebanVanegas <87554378+StebanVanegas@users.noreply.github.com>
2022-05-04 01:56:06 -0500

    Se agregó HuellaActivity
    para hacer el funcionamiento biométrico del sensor de huella.

---
+++ app/src/main/java/com/gitgud/fitpal/Splash.java
@@ -0,0 +1,64 @@
+package com.gitgud.fitpal;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class Splash extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(user==null){
+                    db.collection("Usuario")
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            Log.d(TAG, document.getId() + " => " + document.getData().get("correo"));
+                                        }
+                                    } else {
+                                        Log.w(TAG, "Error getting documents.", task.getException());
+                                    }
+                                }
+                            });
+                    startActivity(new Intent(Splash.this, RegisterActivity.class));
+                    finish();
+                }
+                //else if(){
+                //}
+
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/gitgud/fitpal/data/Deporte.java
@@ -0,0 +1,9 @@
+package com.gitgud.fitpal.data;
+
+public class Deporte {
+    private String ID;
+    private String diminutivo;
+    private String nombre;
+    private String tipo;
+
+}
