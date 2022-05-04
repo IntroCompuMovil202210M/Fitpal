49c9e0fd3f8c42081122e7cafe9d5006287c25a7
Jerson Osorio <61725276+Velorio@users.noreply.github.com>
2022-05-04 03:21:30 -0500

    prueba

--- FitPal/app/src/main/java/com/gitgud/fitpal/Splash.java
+++ FitPal/app/src/main/java/com/gitgud/fitpal/Splash.java
@@ -48,7 +48,7 @@ public class Splash extends AppCompatActivity {
                     startActivity(new Intent(Splash.this, LoginActivity.class));
                     finish();
                 }
-                else{
+                else {
                     db.collection("Usuario")
                             .get()
                             .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -56,12 +56,32 @@ public class Splash extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                     if (task.isSuccessful()) {
                                         for (QueryDocumentSnapshot document : task.getResult()) {
-                                            if(user.getEmail().equals(document.getData().get("correo"))){
-                                                if((boolean) document.getData().get("registroCompleto")){
+                                            if (user.getEmail().equals(document.getData().get("correo"))) {
+
+                                                if ((boolean) document.getData().get("registroCompleto")) {
+                                                    ArrayList<String> eventos = (ArrayList<String>) document.getData().get("eventos");
+                                                    for (String evento : eventos) {
+                                                        DocumentReference docRef = db.collection("eventos").document(evento);
+
+                                                        Source source = Source.CACHE;
+
+                                                        // Get the document, forcing the SDK to use the offline cache
+                                                        docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    // Document found in the offline cache
+                                                                    DocumentSnapshot document = task.getResult();
+                                                                    Log.d(TAG, "Cached document data: " + document.getData());
+                                                                } else {
+                                                                    Log.d(TAG, "Cached get failed: ", task.getException());
+                                                                }
+                                                            }
+                                                        });
+                                                    }
                                                     startActivity(new Intent(Splash.this, MainActivity.class));
                                                     finish();
-                                                }
-                                                else{
+                                                } else {
                                                     startActivity(new Intent(Splash.this, CompleteRegister.class));
                                                     finish();
                                                 }
@@ -73,50 +93,8 @@ public class Splash extends AppCompatActivity {
                                 }
                             });
 
-                    //borrar esto despues
 
-                    String correo = user.getEmail();
-                    ArrayList<String> eventos = new ArrayList<>();
-                    ArrayList<Usuario> usuarios = null;
-                    db.collection("usuarios")
-                            .whereEqualTo("correo", correo)
-                            .get()
-                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                    if (task.isSuccessful()) {
-                                        for (QueryDocumentSnapshot document : task.getResult()) {
-                                            usuarios.add(document.toObject(Usuario.class));
-                                        }
-                                    } else {
-                                        Log.d(TAG, "Error getting documents: ", task.getException());
-                                    }
-                                }
-                            });
-                    eventos = usuarios.get(0).getEventos();
-
-                    for(String evento: eventos){
-                        DocumentReference docRef = db.collection("eventos").document(evento);
-
-                        Source source = Source.CACHE;
-
-                    // Get the document, forcing the SDK to use the offline cache
-                        docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    // Document found in the offline cache
-                                    DocumentSnapshot document = task.getResult();
-                                    Log.d(TAG, "Cached document data: " + document.getData());
-                                } else {
-                                    Log.d(TAG, "Cached get failed: ", task.getException());
-                                }
-                            }
-                        });
-                    }
-                    //borrar esto despues
                 }
-
             }
         },3000);
     }
