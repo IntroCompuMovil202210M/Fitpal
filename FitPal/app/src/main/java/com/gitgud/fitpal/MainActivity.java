22908fd48a371588f2263c216780ee05a02f398a
pablo-savp <santanderlavarez.pablo@gmail.com>
2022-05-03 22:55:22 -0500

    Se añadio clase de Mapa y dependencias pertinentes.

--- FitPal/app/src/main/java/com/gitgud/fitpal/MainActivity.java
+++ FitPal/app/src/main/java/com/gitgud/fitpal/MainActivity.java
@@ -2,7 +2,10 @@ package com.gitgud.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         menuInferior = findViewById(R.id.menuInferior);
         getSupportActionBar().hide();
     }
+
+
 }
--- FitPal/app/src/main/java/com/gitgud/fitpal/RegisterActivity.java
+++ FitPal/app/src/main/java/com/gitgud/fitpal/RegisterActivity.java
@@ -2,13 +2,29 @@ package com.gitgud.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Button botonMapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //botonMapa = findViewById(R.id.button);
+        //botonMapa.setOnClickListener(abrirMapa);
     }
+
+    /*private View.OnClickListener abrirMapa = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(view.getContext(), Mapa.class);
+            startActivity(intent);
+        }
+    };*/
 }
